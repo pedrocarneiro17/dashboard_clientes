429b0224a8adcb754c21ab17e744468e75b0b2c8
Pedro Carneiro <pedro.aco@ufop.aluno.edu.br>
2025-07-23 09:08:53 -0300

    ajuste porta

--- app.py
+++ app.py
@@ -225,4 +225,6 @@ def delete_company(nome_empresa):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host='0.0.0.0', port=port)
